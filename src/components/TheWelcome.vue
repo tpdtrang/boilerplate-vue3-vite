a8ef240e00a693b8e6f92b6faebbe98d9bf80379
Trang <trang.truong@tomosia.com>
2024-10-08 13:26:36 +0700

    init commit

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
   <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
     <template #heading>Documentation</template>
 
     Vue’s
@@ -20,9 +12,6 @@ import SupportIcon from './icons/IconSupport.vue'
   </WelcomeItem>
 
   <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
     <template #heading>Tooling</template>
 
     This project is served and bundled with
@@ -42,9 +31,6 @@ import SupportIcon from './icons/IconSupport.vue'
   </WelcomeItem>
 
   <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
     <template #heading>Ecosystem</template>
 
     Get official tools and libraries for your project:
@@ -58,9 +44,6 @@ import SupportIcon from './icons/IconSupport.vue'
   </WelcomeItem>
 
   <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
     <template #heading>Community</template>
 
     Got stuck? Ask your question on
@@ -76,9 +59,6 @@ import SupportIcon from './icons/IconSupport.vue'
   </WelcomeItem>
 
   <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
     <template #heading>Support Vue</template>
 
     As an independent project, Vue relies on community backing for its sustainability. You can help
