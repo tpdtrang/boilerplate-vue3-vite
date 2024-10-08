ab77ab795299d73022a606b9e11c446477e5fd7a
Trang <trang.truong@tomosia.com>
2024-10-08 16:17:19 +0700

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.ts'
+      },
+      output: {
+        // Cấu hình để tránh tạo ra các file không cần thiết
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor' // Tạo chunk cho các module từ node_modules
+          }
+        }
+      }
+    },
+    emptyOutDir: true
   }
 })
